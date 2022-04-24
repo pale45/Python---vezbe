74ec0240c591686f2e7e383c5f52d32f9fc5f10c
Stefan Palevic <stefan.palevic@24x7.co.yu>
2022-04-24 20:37:20 +0200

    Dodan brzi kviz

---
+++ quick_quizz.py
@@ -0,0 +1,64 @@
+##Kviz znanja - Python vezba
+
+print("Dobrodosli u brzi kviz znanja!")
+
+playing = input ("Da li ste spremni za pocetak? ")
+
+if playing.lower() != "da":
+    quit()
+
+print("Super! Ajde da pocnemo :) ")
+score = 0
+
+answer = input ("Koji je glavni grad Norveske? ")
+
+if answer.lower() == "oslo":
+    print("Tacan odgovor!")
+    score += 1
+else:
+    print('Netacan odgovor :( ')
+
+
+
+answer = input ("Koliko iznosi broj Pi (dve decimale)? ")
+
+if answer == "3.14":
+    print("Tacan odgovor!")
+    score += 1
+else:
+    print('Netacan odgovor :( ')
+
+
+
+answer = input ("Na koliko metara visine se nalazi obruc u kosarci (dve decimale)? ")
+
+if answer == "3.05":
+    print("Tacan odgovor!")
+    score += 1
+else:
+    print('Netacan odgovor :( ')
+
+
+
+answer = input ("Kako se zove poznati muzej u Parizu? ")
+
+if answer.lower() == "luvr":
+    print("Tacan odgovor!")
+    score += 1
+else:
+    print('Netacan odgovor :( ')
+
+
+
+answer = input ("Posle koliko godina se slavi zlatna svadba? ")
+
+if answer == "50":
+    print("Tacan odgovor!")
+    score += 1
+else:
+    print('Netacan odgovor :( ')
+
+
+print("Bravo tacno si odgovorio na " + str(score) + " pitanja! " )
+print("Procenat tacnih odgovora je: " +str((score)/5 *100) + "%")
+
